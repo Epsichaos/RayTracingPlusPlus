533676d681f648a71d94cd9543ff0ed6611249cb
Epsichaos <constant.camille.chaos@gmail.com>
2015-01-20 09:18:43 +0100

    début de la fonction de saisie

---
+++ scene.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+void Scene::load_scene(const string path) {
+
+    int numberOfObjects = 0;
+    int line_size = 0;
+    int i;
+    string file_name = "input";
+    ifstream file_img;
+    file_img.open(file_name.c_str());
+
+    // Si ouverture OK
+    if(file_img) {
+        string line_img;
+        while(getline(file_img, line_img)) {
+            numberOfObjects++;
+            line_size = line_img.size();
+
+        }
+    }
+    // Si erreur
+    else {
+        cerr << "Error when opening input file" << endl;
+    }
+    return 0;
+}
