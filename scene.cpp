edcbb9d72e0c7238dbee9644dc6714903d7514f1
Epsichaos <constant.camille.chaos@gmail.com>
2015-01-30 20:29:20 +0100

    scene

--- scene.cpp
+++ scene.cpp
@@ -13,6 +13,9 @@ void Scene::load_scene(const string path) {
     int nb_light = 0;
     int line_size = 0;
     int i,j;
+
+    string nb_1, nb_2, nb_3;
+
     string file_name = "input";
     ifstream file_img;
     file_img.open(file_name.c_str());
@@ -36,14 +39,30 @@ void Scene::load_scene(const string path) {
                 nb_light++;
             }
         }
+        object *arrayOfObjects = new object[numberOfObjects];
         file_img.seekg(0, ios::beg);
         while(getline(file_img, line_img)) {
             line_size = line_img.size();
             for(i=0; i<line_size; i++) {
                 if(line_img[0]="s"&&line_img[1]="p") {
-                    j = 4;
+                    j = 5;
                     Sphere spr;
-                    //Sphere.setCenter
+                    while(line_img[j]!=" ") {
+                        nb_1 = nb_1 + line_img[j];
+                        j++;
+                    }
+                    j++;
+                    while(line_img[j]!=" ") {
+                        nb_2 = nb_2 + line_img[j];
+                        j++;
+                    }
+                    j++;
+                    while(line_img[j]!=" ") {
+                        nb_3 = nb_3 + line_img[j];
+                        j++;
+                    }
+                    point center(atoi(nb_1), atoi(nb_2), atoi(nb_3));
+                    spr.setCenter(center);
                 }
             }
             // si la ligne est de type sphere
