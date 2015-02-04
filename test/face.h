456392b2b951ecaf48c1ddbcc792a36e31cdc8d9
Epsichaos <constant.camille.chaos@gmail.com>
2015-02-04 10:22:00 +0100

    progression dans la fonction de chargement et tests

---
+++ test/face.h
@@ -0,0 +1,13 @@
+#ifndef FACE_H
+#define FACE_H
+
+class Face
+{
+protected:
+	point m_p1;
+	point m_p2;
+	point m_p2;
+	material m_m1;
+};
+
+#endif
---
+++ test/scene.h
@@ -0,0 +1,17 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+class Scene
+{
+public:
+	void loadScene(const string path);
+	int getNumberOfObjects();
+	int getNumberOfObjects(enum m_type type);
+	void getObjects(object* input_array);
+	void getObjects(object* input_array, enum m_type type);
+protected:
+	m_objectNumber;
+	object* arrayOfObjects;
+};
+
+#endif
