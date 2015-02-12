e68ae3ab6444f7b2fd73ad71fabc922842095b0c
titif26 <titif-26@hotmail.fr>
2015-02-12 15:44:36 +0100

    implémentation de plusieurs classes (suite) et début du tableau qui contient la position des pixels dans l'espace 3D

---
+++ ray.cpp
@@ -0,0 +1,20 @@
+vector Ray::getDirection()
+{
+	return m_direction;
+}
+void Ray::setStart(Point const A)
+{
+	m_start = A;
+}
+void Ray::setDirection(Vector const A)
+{
+	m_direction = A;
+}
+Ray::Ray()
+{
+
+}
+Ray::Ray(Point const A, Vector const B):m_start(A),m_direction(B)
+{
+
+}
