64efb9763843cdcb0b922004af29c42d5c65e8db
Epsichaos <constant.camille.chaos@gmail.com>
2015-02-22 17:48:23 +0100

    début de la doc tmtc

--- object.h
+++ object.h
@@ -7,18 +7,44 @@
 #include "struct.h"
 #include <string>
 
+/*! \class Object
+* \brief Class used to store objects.
+* Objects can be everything : sphere, cubes, or whatever. This class will most of the time not be used. We will use the
+* derivatives classes from the class Object.
+*/
 class Object
 {
 public:
-	Color getColor(Point input_point);
+	/*!
+	* \brief Accessor to the color of an object
+	* \return The color of the object
+	*/
 	Color getColor();
+	/*!
+	* \brief Accessor to the type of an object
+	* \return The string of the type of the object : *camera*, *light*, *sphere* or *cube*
+	*/
 	std::string getType(std::string input);
+	/*!
+	* \brief Set the color of the object
+	* \param color. Set **color** as the new color of the object
+	*/
 	void setColorObject(Color);
+	/*!
+	* \brief Set the type of the object
+	* \param Str. Str can be *camera*, *sphere*, *cube* or *light*
+	*/
 	void setTypeObject(std::string);
 protected:
 	//type_object m_type;
 	//enum m_type {Camera, Light, Cube, Sphere, Mesh};
+	/*!
+	* \brief Int which give the type of object. By convention *camera* = 0, *light* = 1, *cube* = 2, *sphere* = 3 and *mesh* = 4
+	*/
 	int m_type;
+	/*!
+	* \brief Describe the type of material of the object. The color, and the refraction and reflexion values
+	*/
 	Material m_mat;
 };
 
--- scene.h
+++ scene.h
@@ -8,28 +8,65 @@
 
 /*! \class Scene
 * \brief Contains the main scene description and all the objects included in.
-*
+* This class will be used to store all the objects present in the input file.
 */
 class Scene
 {
 public:
+	/*! \brief Constructor of the Scene class.
+	* \param *path* of the input file
+	*/
 	Scene(const std::string path);
+	/*! \brief Accessor to the objects
+	* \return The number of all the objects
+	*/
 	int getNumberOfObjects();
+	/*! \brief Accessor to one specific type of object
+	* \param str a string which contains the name of the class. **str** can be *light*, *camera*, *cube* or *sphere*
+	* \return The number of the objects of one specific class
+	*/
 	int getNumberOfObjects(std::string);
 	// fonction de test pour débugger
 	void debugTest();
 	//void getObjects(Object* input_array);
 	//void getObjects(Object* input_array, enum m_type type);
 protected:
+	/*!
+	* \brief Number of all the objects
+	*/
 	int m_objectNumber;
+	/*!
+	* \brief Number of all the sphere objects
+	*/
 	int m_sphereNumber;
+	/*!
+	* \brief Number of all the camera objects
+	*/
 	int m_cameraNumber;
+	/*!
+	* \brief Number of all the cube objects
+	*/
 	int m_cubeNumber;
+	/*!
+	* \brief Number of all the ligth objects
+	*/
 	int m_lightNumber;
 	//Object* m_arrayOfObjects;
+	/*!
+	* \brief Array which contains all the sphere objects
+	*/
 	Sphere *m_arrayOfSphere;
+	/*!
+	* \brief Array which contains all the cube objects
+	*/
 	Cube *m_arrayOfCube;
+	/*!
+	* \brief Array which contains all the camera objects
+	*/
 	Camera *m_arrayOfCamera;
+	/*!
+	* \brief Array which contains all the light objects
+	*/
 	Light *m_arrayOfLight;
 };
 
